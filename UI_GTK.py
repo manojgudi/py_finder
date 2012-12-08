0413ad2462121755244bac9311ffcd5a4e2fe434
manojgudi <manoj.p.gudi@gmailo.com>
2012-12-08 22:14:28 +0530

    added feature: 1> florence keryboard begins at inception of app 2> resolved the gtk-builder issue

--- UI_GTK.py
+++ UI_GTK.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 
 import sys
+import subprocess as sp
 
 try:
 	import pygtk
@@ -11,7 +12,7 @@ except:
 	
 try:
 	import gtk
-	import gtk.glade
+	import gtk.glade 
 except:
 	print "check gtk deps"
 	sys.exit()
@@ -22,16 +23,37 @@ class front_end:
 	def __init__(self):
 	
 		#Set glade file
-		self.gladefile = "UI_GTK.glade"
-		self.wTree = gtk.glade.XML(self.gladefile)
-	
-		# Get main window and connect "destroy" event
-		self.window = self.wTree.get_widget("MainWindow")
-		if (self.window):
-			self.window.connect("destroy", gtk.main_quit)
+		self.gladefile = "/home/vm/scripts/py_finder/UI_GTK.glade"
+		self.glade = gtk.Builder()
+		self.glade.add_from_file(self.gladefile)
+		
 
+        	self.window=self.glade.get_object("window1")
+                self.window.show_all()
+                self.open_kybd()
+                self.dic = {"on_apps_searchbox_insert_at_cursor" : self.on_apps_searchbox_activate, "on_MainWindow_destroy" : gtk.main_quit }
+                self.glade.connect_signals(self.dic)
+
+
+        def open_kybd(self):
+                try:
+                        sp.Popen("florence")
+                        print "started florence onscreen keyboard"
+                except:
+                        print "florence not found"
+                        
+                
+	def main(self):		
+		print 'hello dick'
+
+        
+        def on_apps_searchbox_activate(self,widget):
+       		print "its working biatch"
+		
+
+	
 
 if __name__=="__main__" :
 	front_obj=front_end()
-	gtk.main()
-  
+	front_obj.main()
+        gtk.main()
