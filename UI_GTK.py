13aa99781a3116693186dd1aa92c94c6d48bd2e2
manojgudi <manoj.p.gudi@gmailo.com>
2012-12-09 17:39:53 +0530

    fix to quit properly and unload florence

--- UI_GTK.py
+++ UI_GTK.py
@@ -16,39 +16,46 @@ try:
 	import gtk.glade 
 except:
 	print "check gtk deps"
-	sys.exit()
-
+	sys.exit(1)
 
 class front_end:
 	""" This is front handler for appsearch feature"""
 	def __init__(self):
 	
 		#Set glade file
-		self.gladefile = "/home/vm/scripts/py_finder/UI_GTK.glade"
+		self.gladefile = "./UI_GTK.glade"
 		self.glade = gtk.Builder()
 		self.glade.add_from_file(self.gladefile)
 		
-
-        	self.window=self.glade.get_object("window1")
-                self.window.show_all()
-                self.open_kybd()
-                self.dic = {"on_apps_searchbox_activate" : self.on_apps_searchbox_activate, "on_window1_destroy_event" : gtk.main_quit }
-                self.glade.connect_signals(self.dic)
-
-
+		self.window=self.glade.get_object("window1")
+		self.window.show_all()
+		self.window.connect("destroy",self.gtk_main_quit)
+		self.open_kybd()
+		self.dic = {"on_apps_searchbox_activate" : self.on_apps_searchbox_activate, "destroy" : self.gtk_main_quit }
+		self.glade.connect_signals(self.dic)
+		
         def open_kybd(self):
                 try:
                         sp.Popen("florence")
                         print "started florence onscreen keyboard"
                 except:
                         print "florence not found"
-                        
+
+	def gtk_main_quit(self,widget):
+		"""
+			Predefined callback.
+			Equivalent to self.quit()
+			I want my ram free of any infections after fucking with GTK....;)
+		"""                    
+		sp.Popen(["echo", "Killing florence"])
+		sp.Popen(["pkill", "florence"])			
+		gtk.main_quit()
                 
 	def main(self):		
 	# Insert any code just before apps goes into gtk.main()
-		print 'hello dick'
+		print 'manoj is a dick'
 
-        
+
         def on_apps_searchbox_activate(self,widget):
        		print "its working biatch"
        		self.apps_searchbox=self.glade.get_object("apps_searchbox")
@@ -75,10 +82,8 @@ class front_end:
 			self.apps_display_result.set_text(self.output)
 			self.apps_searchbox.set_text("")
 
-	
-	
 
 if __name__=="__main__" :
 	front_obj=front_end()
 	front_obj.main()
-        gtk.main()
+    	gtk.main()
