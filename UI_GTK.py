51a35fe9886ea65c0dc2d1ee0bd949739559545f
Pranav Salunke <pps.pranav@gmail.com>
2013-05-11 13:30:56 +0530

    Added License to the Source
    For good practice and also for if someone is using our code (ahem ahem ... :D) It is better and advised to add this section here.

--- UI_GTK.py
+++ UI_GTK.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env/python
+'''
+    This is small application designed to run on LXDE to search applications 
+    and files on local drive. For more information check out :
+    		http://manojgudi.github.io/py_finder/
+    Copyright (C) 2012/2013 Manoj Gudi, Pranav Salunke
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
 
 import sys
 import subprocess as sp
